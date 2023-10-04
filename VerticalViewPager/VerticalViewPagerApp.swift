7a14b09494ae04eee515c90919f178ca4ea2dd55
Xing <zhaoxing8325@gmail.com>
2023-10-03 22:52:20 -0700

    Initial Commit

---
+++ VerticalViewPager/VerticalViewPagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VerticalViewPagerApp.swift
+//  VerticalViewPager
+//
+//  Created by XING ZHAO on 2023-10-03.
+//
+
+import SwiftUI
+
+@main
+struct VerticalViewPagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
